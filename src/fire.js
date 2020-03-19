f1645b86be1f3abaececc8e2544e57ad32b4d423
Ji Hye Park <jhp0621@gmail.com>
2020-03-19 10:45:36 -0500

    with firebase set up

---
+++ src/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrRKLyIIZKEtsOYIO1h21INZn_1GnzcrM",
+  authDomain: "capstone-a9c19.firebaseapp.com",
+  databaseURL: "https://capstone-a9c19.firebaseio.com",
+  projectId: "capstone-a9c19",
+  storageBucket: "capstone-a9c19.appspot.com",
+  messagingSenderId: "327054793685",
+  appId: "1:327054793685:web:293cf006d00f076b6e3b5c",
+  measurementId: "G-73386QC55Y"
+};
+
+const fire = firebase.initializeApp(firebaseConfig)
+
+export default fire
